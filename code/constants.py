90de15a61d85c4938236f0e79feea78b30c97151
Tobias Broeckmann <tobias.broeckmann@rwth-aachen.de>
2025-04-29 19:30:13 +0200

    FeatureGenOnCluster

--- code/constants.py
+++ code/constants.py
@@ -1,5 +1,7 @@
 import torch
 
+# Some default constant values. Most variables can be changed in the config files if desired, the values in this file are considered default values. 
+
 # If not specified, the seed below is used
 SEED = 37
 
@@ -14,8 +16,8 @@ vector_buffer_size = 16_384
 # Used for lovasz features
 num_lo_gens = 2 # Number of times the Lovasz features are generated in order to 
 sdp_max_iters = 20 # maximum number of iterations done in the sdp solver used in computing the lo-theta value of a graph
-lo_small_val = float("1e-13") # Used to make the zero matrix pos definite if necessary
-welzl_tolerance = float("1e-10")
+lo_small_val = float("1e-13") # Used to make the zero matrix pos definite if necessary to compute a cholesky decomposition
+welzl_tolerance = float("1e-10") # Tolerance used in Welzls algorithm to decide whether a point is a border point of a disk
 
 # Used in the experiments
 num_workers = 0 # The number of worker processes used in the dataloaders for gnn training, 0 means not utilising parallelization in this step
@@ -25,10 +27,9 @@ k_fold_test_ratio = 0.3 # used for CSL experiments
 max_patience = 100 # patience value used in gnn training. If the validation performance does not improve for max_patience training steps, the training is prematurely halted.
 use_batch_norm = False # Specifiy whether batch normalization is utilised in gnn training.
 # clustering
-mbk_batch_size = 1024
+mbk_batch_size = 2048
 mbk_n_init = 3
 mbk_max_no_improvement = 10
-mbk_max_iter = 100
+mbk_max_iter = 500
 
-# device = torch.device('cpu')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
